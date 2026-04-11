a48bcb3aa4e0cc62d1b8a203386a7b2fa0006121
kiso ryuhei <uw2374@icloud.com>
2026-04-11 20:56:58 +0900

    reset

--- setup/gui/src-tauri/build.rs
+++
@@ -1,6 +0,0 @@
-// Tauriのビルドスクリプト。ビルド時のTauri固有の設定処理を行う。
-
-fn main() {
-    // Tauriビルドの初期化処理を実行する
-    tauri_build::build()
-}
--- setup/gui/src/test/setup.ts
+++
@@ -1,4 +0,0 @@
-// Vitestのグローバルセットアップファイル。jest-domマッチャーを拡張する。
-
-// jest-domのカスタムマッチャーをインポートして有効化する
-import "@testing-library/jest-dom";
